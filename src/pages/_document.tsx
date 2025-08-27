bc6eda16a7020c6175723a07af85ef98149aaf93
Felipe Oliveira <felpd3v@gmail.com>
2025-08-26 21:43:17 -0300

    chore: html mobile updates

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
 	return (
 		<Html lang='en'>
 			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 				<title>Felipe Oliveira - Software Engineer</title>
 				<link rel='shortcut icon' href='/favicon.png' type='image/png' />
 			</Head>
